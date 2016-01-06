88e79b4cab87fc5b35dbbc2b878891007c33efcd
kiwenlau <kiwenlau@126.com>
2016-01-06 16:08:27 +0900

    comment docker build

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo -e "\nbuild kubernetes image...\n"
-cd kubernetes
-sudo docker build -t kiwenlau/kubernetes-supervisor:1.0.7 .
+# echo -e "\nbuild kubernetes image...\n"
+# cd kubernetes
+# sudo docker build -t kiwenlau/kubernetes-supervisor:1.0.7 .
 
 docker rm -f kubernetes > /dev/null
 
-echo -e "\nstart kubernetes container, please wait for 10 seconds..."
+echo -e "\nstart kubernetes container..."
 docker run -it -d --privileged --name=kubernetes --hostname=kubernetes kiwenlau/kubernetes-supervisor:1.0.7 >/dev/null
 
 echo -e "\nget into kubernetes container\n"
